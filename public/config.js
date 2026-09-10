bae732b11f1ba52c0ddd9c0411f0417aad184dd5
Fl0rim123 <122523355+Fl0rim123@users.noreply.github.com>
2026-09-10 16:39:29 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.WOMM_CONFIG = {
   environment: "PRODUCTION",
-  version: "1.0.0",
-  developerConfidence: "73%",
-  statusText: "Probably fine."
+  version: "1.1.0",
+  developerConfidence: "67%",
+  statusText: "It just works."
 };
